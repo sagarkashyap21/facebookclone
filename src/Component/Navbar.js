14ecdf1d5c33772a70f1fd23a4d2523e806b9760
sagar kashyap <kashyapsagarks21@gmail.com>
2023-01-19 12:16:05 +0530

    fifth commit

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -9,19 +9,19 @@ export default function Navbar() {
      <nav className="nav1" style={{display:"flex",justifyContent:"space-between"}} >
        <h1 style={{color:"blue",marginLeft:"50px",marginTop:"4px"}}>facebook</h1>
        <ul style={{listStyle:"none",display:"flex",marginRight:"100px",marginTop:"20px"}} >
-         <li style={{border:"0px solid gray",fontSize:"20px",borderRadius:"50px",backgroundColor:"lightgray"}}><a href=""><i class="fa fa-solid fa-magnifying-glass" style={{color:"black",padding:"5px"}}></i></a></li>
-         <li style={{border:"0px solid gray",fontSize:"20px",borderRadius:"50px",backgroundColor:"lightgray",marginLeft:"20px"}}><a href=""><i class="fa fa-sharp fa-solid fa-bars" style={{color:"black",padding:"5px"}}></i></a></li>
+         <li style={{border:"0px solid gray",fontSize:"20px",borderRadius:"50px",backgroundColor:"lightgray"}}><a for=""><i class="fa fa-solid fa-magnifying-glass" style={{color:"black",padding:"5px"}}></i></a></li>
+         <li style={{border:"0px solid gray",fontSize:"20px",borderRadius:"50px",backgroundColor:"lightgray",marginLeft:"20px"}}><a for=""><i class="fa fa-sharp fa-solid fa-bars" style={{color:"black",padding:"5px"}}></i></a></li>
        </ul>
      </nav>
 
      <nav style={{}}>
        <ul style={{listStyle:"none",display:"flex",alignItems:"center",justifyContent:"space-around"}}>
-         <li  style={{fontSize:"20px"}}><a href="/"><i style={{color:"gray"}} class="fa fa-solid fa-house"></i></a></li>
-         <li style={{fontSize:"20px"}}><a href="/friends"><i style={{color:"gray"}} class="fa fa-solid fa-user-group"></i></a></li>
-         <li style={{fontSize:"20px"}}><a href="/message"><i  style={{color:"gray"}}class="fa-solid fa-message"></i></a></li>
-         <li style={{fontSize:"20px"}}><a href="/bell"><i style={{color:"gray"}} class="fa fa-solid fa-bell"></i></a></li>
-         {/* <li style={{fontSize:"20px"}}><a href="/videos"><i style={{color:"gray"}} class="fa-solid fa-tv"></i></a></li> */}
-         <li style={{fontSize:"20px"}}><a href="/shop"><i style={{color:"gray"}} class="fa fa-solid fa-shop"></i></a></li>
+         <li  style={{fontSize:"20px"}}><a for="/"><i style={{color:"gray"}} class="fa fa-solid fa-house"></i></a></li>
+         <li style={{fontSize:"20px"}}><a for="/friends"><i style={{color:"gray"}} class="fa fa-solid fa-user-group"></i></a></li>
+         <li style={{fontSize:"20px"}}><a for="/message"><i  style={{color:"gray"}}class="fa-solid fa-message"></i></a></li>
+         <li style={{fontSize:"20px"}}><a for="/bell"><i style={{color:"gray"}} class="fa fa-solid fa-bell"></i></a></li>
+         {/* <li style={{fontSize:"20px"}}><a for="/videos"><i style={{color:"gray"}} class="fa-solid fa-tv"></i></a></li> */}
+         <li style={{fontSize:"20px"}}><a for="/shop"><i style={{color:"gray"}} class="fa fa-solid fa-shop"></i></a></li>
        </ul>
      </nav>
        <hr />
